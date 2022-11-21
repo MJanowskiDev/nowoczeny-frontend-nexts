3b7ddd0b3e87a9c10de97ccf42800d484e937c04
MJanowskiDev <mat.krz.janowski@gmail.com>
2022-11-21 20:38:02 +0100

    Added react query to newsletter form

--- components/NewsletterForm.tsx
+++ components/NewsletterForm.tsx
@@ -1,6 +1,20 @@
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
+import { useMutation } from "react-query";
 import * as yup from "yup";
+
+const useAddToNewsletterMutation = () => {
+  return useMutation(
+    "add-to-newsletter",
+    async ({ email }: { email: string }) => {
+      await fetch("/api/subscribe", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email }),
+      });
+    }
+  );
+};
 const newsletterFormSchema = yup
   .object({
     email: yup.string().required().email(),
@@ -15,16 +29,18 @@ export const NewsletterForm = () => {
       resolver: yupResolver(newsletterFormSchema),
     });
 
-  const onSubmit = handleSubmit(async (data) => {
-    await fetch("/api/subscribe", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
-    });
+  const { mutate, isLoading, isSuccess, isError } =
+    useAddToNewsletterMutation();
+  const onSubmit = handleSubmit((data) => {
+    mutate(data);
   });
 
   return (
     <>
+      {isLoading && <p>Is loading</p>}
+      {isError && <p>Is error</p>}
+      {isSuccess && <p>isSuccess</p>}
+
       <form onSubmit={onSubmit}>
         <div>
           <label className="mb-1 block text-sm text-gray-600" htmlFor="email">
